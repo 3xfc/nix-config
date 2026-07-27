7cf9ecc0c6ff1655070ffcea129d64d7904cbd11
Peter Marshall <pm@petermarshall.ca>
2026-07-27 15:26:18 -0400

    update nod

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-on-droid = {
-      url = "github:petm5/nix-on-droid/proot-optimization";
+      url = "github:petm5/nix-on-droid/idiomatic";
       # url = "git+file:///home/petms/nix-on-droid/";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
